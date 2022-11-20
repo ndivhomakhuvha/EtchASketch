2b926c062c3814afbf4ebf53db4681d17907d9fb
Ndivho69 <ndibo69@gmail.com>
2022-11-21 01:01:38 +0200

    added erase

--- public/script/index.js
+++ public/script/index.js
@@ -1,59 +1,73 @@
-const container = document.querySelector('.drawingBoard');
-const color = document.querySelector('.color');
-const slider = document.querySelector('.slider')
-let length, mousedown = false, colorName;
+const container = document.querySelector(".drawingBoard");
+const color = document.querySelector(".color");
+const eraser = document.querySelector(".eraser");
+const eraserContainer = document.querySelector(".eraserContainer");
+const slider = document.querySelector(".slider");
+let length,
+  mousedown = false,
+  colorName;
 
-
+function Eraser() {
+  switch (eraser.style.color) {
+    case "red":
+      colorName = "";
+      eraser.style.color = "green";
+      eraserContainer.style.backgroundColor = `rgba(73, 172, 73, 0.315)`;
+      break;
+    default:
+      colorName = "white";
+      eraser.style.color = "red";
+      eraserContainer.style.backgroundColor = "rgba(255, 0, 0, 0.295)";
+  }
+}
 function setGrid(length) {
-    container.style.display = 'grid';
-    container.style.gridTemplateColumns = `repeat(${length},1fr)`
-    container.style.gridTemplateRows = `repeat(${length},1fr)`
+  container.style.display = "grid";
+  container.style.gridTemplateColumns = `repeat(${length},1fr)`;
+  container.style.gridTemplateRows = `repeat(${length},1fr)`;
 }
 
 function isMouseDown() {
-    document.addEventListener('mousedown', function () {
-        mousedown = true;
-    })
-    document.addEventListener('mouseup', function () {
-        mousedown = false;
-    })
+  document.addEventListener("mousedown", function () {
+    mousedown = true;
+  });
+  document.addEventListener("mouseup", function () {
+    mousedown = false;
+  });
 }
-
 
 function changeColor() {
-    color.addEventListener('change', function (e) {
-        colorName = this.value;
-    })
-
+  color.addEventListener("change", function (e) {
+    colorName = this.value;
+  });
 }
 function mouseAction() {
-    if (mousedown) {
-        if (colorName) {
-            this.style.background = `${colorName}`
-        }
-    } else {
-        return;
+  if (mousedown) {
+    if (colorName) {
+      this.style.background = `${colorName}`;
     }
+  } else {
+    return;
+  }
 }
 function creatingDiv() {
-    container.textContent = '';
-    for (let i = 1; i <= length * length; i++) {
-        let div = document.createElement('div');
-        div.classList.add('box')
-        container.appendChild(div)
-        div.addEventListener('mouseenter', mouseAction)
-    }
+  container.textContent = "";
+  for (let i = 1; i <= length * length; i++) {
+    let div = document.createElement("div");
+    div.classList.add("box");
+    container.appendChild(div);
+    div.addEventListener("mouseenter", mouseAction);
+  }
 }
 
 function draw() {
-    isMouseDown();
-    changeColor();
-    creatingDiv();
-
+  isMouseDown();
+  changeColor();
+  creatingDiv();
 }
 
-slider.addEventListener('change', (e) => {
-    length = e.target.value;
-    setGrid(length);
-    draw(length)
-})
+slider.addEventListener("change", (e) => {
+  length = e.target.value;
+  setGrid(length);
+  draw(length);
+});
+eraser.addEventListener("click", Eraser);
